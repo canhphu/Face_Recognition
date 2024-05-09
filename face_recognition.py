bf1695141250d62d95dc65d1bc0aa016f675b2c5
canhphu <canhphu131204@gmail.com>
2024-05-09 16:12:19 +0700

    Demo Face Recognition

--- face_recognition.py
+++ face_recognition.py
@@ -1,6 +1,53 @@
-from facenet_pytorch import MTCNN
-import cv2
-from PIL import Image
-import numpy as np
-from matplotlib import pyplot as plt
-from tqdm.notebook import tqdm
+import mtcnn
+TF_ENABLE_ONEDNN_OPTS=0
+import matplotlib.pyplot as plt
+# load image from file
+filename = "glediston-bastos-ZtmmR9D_2tA-unsplash.jpg"
+pixels = plt.imread(filename)
+print("Shape of image/array:",pixels.shape)
+imgplot = plt.imshow(pixels)
+plt.show()
+detector = mtcnn.MTCNN()
+# detect faces in the image
+faces = detector.detect_faces(pixels)
+for face in faces:
+    print(face)
+
+
+# draw an image with detected objects
+def draw_facebox(filename, result_list):
+    # load the image
+    data = plt.imread(filename)
+    # plot the image
+    plt.imshow(data)
+    # get the context for drawing boxes
+    ax = plt.gca()
+    # plot each box
+    for result in result_list:
+        # get coordinates
+        x, y, width, height = result['box']
+        # create the shape
+        rect = plt.Rectangle((x, y), width, height, fill=False, color='orange')
+        # draw the box
+        ax.add_patch(rect)
+        # draw the dots
+        for key, value in result['keypoints'].items():
+            # create and draw dot
+            dot = plt.Circle(value, radius=20, color='red')
+            ax.add_patch(dot)
+            # show the plot
+        plt.show()
+    # show the plot
+    plt.show()
+
+
+# filename = 'test1.jpg' # filename is defined above, otherwise uncomment
+# load image from file
+# pixels = plt.imread(filename) # defined above, otherwise uncomment
+# detector is defined above, otherwise uncomment
+# detector = mtcnn.MTCNN()
+# detect faces in the image
+faces = detector.detect_faces(pixels)
+# display faces on the original image
+draw_facebox(filename, faces)
+
