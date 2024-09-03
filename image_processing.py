682dc15e89491b53b0ae433ce41590d08b62d040
Le Dong Canh Phu <canhphu131204@gmail.com>
2024-09-03 20:55:00 +0700

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-import numpy
+
 def extract_labels_from_filename(filename):
     # Remove file extension
-    filename = tf.strings.regex_replace(filename, r'\.jpg$', '')
+    filename = tf.strings.regex_replace(filename, r'\.jpg\.chip\.jpg$', '')
     # Split the filename into parts
     parts = tf.strings.split(filename, '_')
 
@@ -13,22 +13,36 @@ def extract_labels_from_filename(filename):
 
     return age, gender, race
 
-def load_image_and_labels(filename, image_dir):
-    image_path = tf.strings.join([image_dir, filename], separator='/')
-    image = tf.io.read_file(image_path)
+def load_image_and_labels(filename):
+    # Replace backward slashes with forward slashes
+    filename = tf.strings.regex_replace(filename, '\\\\', '/')
+
+    # Load and decode the image
+    image = tf.io.read_file(filename)
     image = tf.image.decode_jpeg(image, channels=3)
+
+    # Resize the image to 200x200 pixels
     image = tf.image.resize(image, [200, 200])
+
+    # Normalize the image to range [0, 1]
     image = image / 255.0
 
-    age, gender, race = extract_labels_from_filename(filename)
+    # Extract labels from the filename
+    age, gender, race = extract_labels_from_filename(tf.strings.split(filename, '/')[-1])
 
     return image, (age, gender, race)
 
 def data_generator(image_dir, batch_size):
+    # Create a dataset of file paths
     file_list = tf.data.Dataset.list_files(image_dir + '/*.jpg')
+
+    # Shuffle the dataset
     dataset = file_list.shuffle(buffer_size=1000)
 
-    dataset = dataset.map(lambda filename: load_image_and_labels(filename, image_dir))
-    dataset = dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    # Map filenames to images and labels
+    dataset = dataset.map(load_image_and_labels)
+
+    # Batch the data and prefetch
+    dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
     return dataset
